8fb1aa6cb5788ddad08e3678747df2f79c14ce20
doctor <doctor.dev.games@gmail.com>
2022-04-07 23:31:57 +0900

    更新

---
+++ Assets/Setsuna/Scripts/Battle/Sample.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+
+namespace Setsuna
+{
+    public class Sample : MonoBehaviour
+    {
+        public void Start()
+        {
+            // 親オブジェクトを生成する
+            親 親 = new 親();
+            // 「私は親」と表示される
+            親.名乗り();
+            // 「私は山口県出身です」と表示される
+            親.住まい();
+
+            親 ダディ = new 親();
+            // 「私は親」と表示される
+            ダディ.名乗り();
+            // 「私は山口県出身です」と表示される
+            ダディ.住まい();
+            // 何が表示される？
+            ダディ.出身国();
+            ダディ.名前 = "ギャレン";
+
+            子 子 = new 子();
+            // 問題:「？」と表示される、？は何
+            子.名乗り();
+            // 問題:「？」と表示される、？は何
+            子.住まい();
+            // 何が表示される？
+            子.出身国();
+            子.名前 = "キッズ";
+
+        }
+
+        public void Update()
+        {
+            // フレーム毎に実行する
+        }
+    }
+
+    public class 親
+    {
+        public string 名前;
+        
+        private string country = "アメリカ";
+
+        public virtual void 名乗り()
+        {
+            Debug.Log("私は親");
+        }
+
+        public virtual void 住まい()
+        {
+            Debug.Log("私は山口県出身です");
+        }
+
+        public virtual void 出身国()
+        {
+            Debug.Log(country);
+        }
+    }
+
+    public class 子 : 親
+    {
+        private string country = "日本";
+
+        public string 名前;
+
+        public void 名前を名乗る()
+        {
+            // 子の名前が表示
+            Debug.Log(名前);
+            // 親の名前が表示
+            Debug.Log(base.名前);
+        }
+
+        public override void 名乗り()
+        {
+            Debug.Log("私は子");
+        }
+
+        public override void 出身国()
+        {
+            Debug.Log(country);
+        }
+    }
+}
--- Assets/Setsuna/Scripts/Common/SceneBase.cs
+++ Assets/Setsuna/Scripts/Common/SceneBase.cs
@@ -7,23 +7,36 @@ namespace Setsuna
 {
     public abstract class SceneBase<T> : MonoBehaviour, IScene where T : ISceneParameter, new()
     {
+        // バトルシーンはここから開始する
         private IEnumerator Start()
         {
+            // 1回きりしか実行しないようにしている
+            // if文は条件式、true(真)である場合、ifの中身を実行する
+            // bool型 = 真偽値　真(true)or偽(false)
+            // ツクール語でいうとスイッチOFFである場合
+            // スイッチON = true, スイッチOFF = false
             if (!SceneManager.Instance.StartInitialize)
             {
+                // StartInitializeが終わったことを記述する
+                // ツクール語でいうとスイッチONにした
                 SceneManager.Instance.StartInitialize = true;
+                // 関数`OnPlay`を実行する
+                // 引数については説明を割愛
                 yield return OnPlay(new T());
             }
         }
 
         public IEnumerator OnPlay(ISceneParameter parameter)
         {
+            // 関数`OnInitialize`を実行する
             OnInitialize();
+            // 関数`OnApply`を実行する
             yield return OnApply(parameter);
         }
 
         public void OnInitialize()
         {
+            // 関数`Initialize`を実行する
             Initialize();
         }
 
