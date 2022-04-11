1422b1cf787b8c6f63938ca096056370e69c67ae
doctor <doctor.dev.games@gmail.com>
2022-04-11 23:21:20 +0900

    更新

--- Assets/Setsuna/Scripts/Battle/Sample.cs
+++ Assets/Setsuna/Scripts/Battle/Sample.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 namespace Setsuna
 {
@@ -6,83 +7,116 @@ namespace Setsuna
     {
         public void Start()
         {
-            // 親オブジェクトを生成する
-            親 親 = new 親();
-            // 「私は親」と表示される
-            親.名乗り();
-            // 「私は山口県出身です」と表示される
-            親.住まい();
+            // カート : カートを定義する
+            Cart mario = new Cart("マリオ", 3, 3, 2, 3);
+            Cart luigi = new Cart("ルイージ", 3, 3, 3, 2);
+            Cart yoshi = new Cart("ヨッシー", 4, 4, 2, 1);
+            Cart peach = new Cart("ピーチ", 4, 3, 3, 1);
+            Cart donkey = new Cart("ドソキーユング", 5, 1, 1, 5);
+            Cart kuppa = new Cart("クッパ", 5, 1, 2, 4);
+            Cart kinopio = new Cart("キノピオ", 5, 5, 5, 1);
+            
+            // マリオと表示
+            Debug.Log(mario.GetName());
+            
+            // 以下２つの結果は何がはいるか
+            Debug.Log(yoshi.GetTotalStatus());
+            Debug.Log(donkey.GetTotalStatus());
 
-            親 ダディ = new 親();
-            // 「私は親」と表示される
-            ダディ.名乗り();
-            // 「私は山口県出身です」と表示される
-            ダディ.住まい();
-            // 何が表示される？
-            ダディ.出身国();
-            ダディ.名前 = "ギャレン";
-
-            子 子 = new 子();
-            // 問題:「？」と表示される、？は何
-            子.名乗り();
-            // 問題:「？」と表示される、？は何
-            子.住まい();
-            // 何が表示される？
-            子.出身国();
-            子.名前 = "キッズ";
-
-        }
-
-        public void Update()
-        {
-            // フレーム毎に実行する
+            // カップ : カップを定義する
+            Cup cup = new Cup("きのこカップ");
+            cup.Entry(mario);
+            cup.Entry(luigi);
+            cup.Entry(yoshi);
+            cup.Entry(peach);
+            cup.Entry(donkey);
+            cup.Entry(kuppa);
+            cup.Entry(kinopio);
+            
+            // レース : レースを定義する
         }
     }
 
-    public class 親
+    public class Cart
     {
-        public string 名前;
+        // 名前
+        public string name;
+
+        // スピード
+        public int speed;
+
+        // グリップ
+        public int grip;
+
+        // 加速
+        public int accelerator;
+
+        // 重量
+        public int weight;
+
+        // コンストラクタの書き方
+        // 修飾子 クラス名(引数)
+        public Cart(string name, int speed, int grip, int accelerator, int weight)
+        {
+            // thisは自分自身、this=クラス
+            this.name = name;
+            this.speed = speed;
+            this.grip = grip;
+            this.accelerator = accelerator;
+            this.weight = weight;
+        }
+
+        // 関数の書き方
+        // 修飾子 型 名前(引数)
+        // public = 修飾子
+        //   public = どこからでもアクセスできる
+        //   protected = 継承先までアクセスOK
+        //   private = 自分自身以外のアクセス禁止
+        // 
+        // void = 型　※voidはないという意味、戻り値がない
+        // 関数 = 関数の名称
+        // () = 引数、()だけだと引数なし
+        // {  }
+        public void 関数()
+        {
+            return;
+        }
+
+        // 修飾子 型 関数名(引数)
+        // この場合、戻り値は「変数 name」になる
+        public string GetName()
+        {
+            // string = 文字列を意味する
+            // return this.name と同等
+            return name;
+        }
+
+        public int GetTotalStatus()
+        {
+            return speed + grip + accelerator + weight;
+        }
+    }
+
+    public class Cup
+    {
+        public string name;
+
+        public List<Cart> entry;
+
+        public void Entry(Cart cart)
+        {
+            entry.Add(cart);
+        }
+
+        public Cup(string name)
+        {
+            this.name = name;
+            entry = new List<Cart>();
+        }
+    }
+
+    public class Race
+    {
         
-        private string country = "アメリカ";
-
-        public virtual void 名乗り()
-        {
-            Debug.Log("私は親");
-        }
-
-        public virtual void 住まい()
-        {
-            Debug.Log("私は山口県出身です");
-        }
-
-        public virtual void 出身国()
-        {
-            Debug.Log(country);
-        }
-    }
-
-    public class 子 : 親
-    {
-        private string country = "日本";
-
-        public string 名前;
-
-        public void 名前を名乗る()
-        {
-            // 子の名前が表示
-            Debug.Log(名前);
-            // 親の名前が表示
-            Debug.Log(base.名前);
-        }
-
-        public override void 名乗り()
-        {
-            Debug.Log("私は子");
-        }
-
-        public override void 出身国()
-        {
-            Debug.Log(country);
-        }
     }
 }
