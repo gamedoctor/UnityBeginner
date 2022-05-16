ac3685d385e07ced850ea241815a08b8564d795d
doctor <doctor.dev.games@gmail.com>
2022-05-16 21:13:51 +0900

    答えだよ

--- Assets/Setsuna/Scripts/Battle/Sample.cs
+++ Assets/Setsuna/Scripts/Battle/Sample.cs
@@ -23,6 +23,67 @@ namespace Setsuna
             // Debug.Log("<color=red>あああ</color>");
             //
             // 種族値は実際のポケモンの数値を参照する。
+
+            var monsters = new List<Monster>
+            {
+                new Monster("ヒトカゲ", 39, 52, 43, 60, 50, 65),
+                new Monster("リザード", 58, 64, 58, 80, 65, 80),
+                new Monster("リザードン", 78, 84, 78, 109, 85, 100),
+                new Monster("ゼニガメ", 44, 48, 65, 50, 64, 43),
+                new Monster("カメール", 59, 63, 80, 65, 80, 58),
+                new Monster("カメックス", 79, 83, 100, 85, 105, 78),
+                new Monster("フシギダネ", 45, 49, 49, 65, 65, 45),
+                new Monster("フシギソウ", 60, 62, 63, 80, 80, 60),
+                new Monster("フシギバナ", 80, 82, 83, 100, 100, 80),
+            };
+
+            ConsoleMonsters(
+                monsters.OrderByDescending(v => v.h),
+                (monster) =>
+                    $"{monster.name}　<color=red>{monster.h}</color>/{monster.a}/{monster.b}/{monster.c}/{monster.d}/{monster.s}"
+            );
+
+            ConsoleMonsters(
+                monsters.OrderByDescending(v => v.c),
+                (monster) =>
+                    $"{monster.name}　{monster.h}/{monster.a}/{monster.b}/<color=red>{monster.c}</color>/{monster.d}/{monster.s}"
+            );
+
+            ConsoleMonsters(
+                monsters.OrderByDescending(v => v.s),
+                (monster) =>
+                    $"{monster.name}　{monster.h}/{monster.a}/{monster.b}/{monster.c}/{monster.d}/<color=red>{monster.s}</color>"
+            );
+        }
+
+        private void ConsoleMonsters(IEnumerable<Monster> monsters, Func<Monster, string> onEvent)
+        {
+            foreach (var monster in monsters)
+            {
+                Debug.Log(onEvent(monster));
+            }
+        }
+
+        public class Monster
+        {
+            public string name;
+            public int h;
+            public int a;
+            public int b;
+            public int c;
+            public int d;
+            public int s;
+
+            public Monster(string name, int hitPoint, int pAttack, int pDefense, int sAttack, int sDefence, int speed)
+            {
+                this.name = name;
+                h = hitPoint;
+                a = pAttack;
+                b = pDefense;
+                c = sAttack;
+                d = sDefence;
+                s = speed;
+            }
         }
     }
 }
