9eb64fb12ab0f3fab37849e275d5e3f859a9d9a3
sirokarasu <rockshooter3931@gmail.com>
2022-05-16 22:48:50 +0900

    編集経過

--- Assets/Setsuna/Scripts/Battle/Sample.cs
+++ Assets/Setsuna/Scripts/Battle/Sample.cs
@@ -23,9 +23,53 @@ namespace Setsuna
             // その上で販売品・非売品の分類にさらに分けてそれぞれ表示させよ
             // また販売品は緑文字、非売品は赤文字で名前を表示すること、そしてカンマ区切りの1行でそれぞれ表示せよ
             // (例) Debug.Log("武器種剣　販売品: A,B 非売品: C");　※種類毎にはLog出力はわけてOK
-            // 
+            // ※上記のようなログの出し方をしたい場合は、出力用のstring型変数を用意すると解決する
+            // 販売品A,Bが入るstring型変数と、非売品Cが入る変数を用意しよう
+            //
             // また予め剣・斧・槍もしくは販売品・非売品などでListをわけることは禁止で、必ずひとつのListから始めること
             // 結果が複数のListになることは許容するが、最大3つまでとする
+            // ※関数の中で一時変数としてリストを作れば丸いかもしれない
+
+            List<WeaponBase> weaponlist = new List<WeaponBase>();
+
+            Sword bronssword = new Sword(name = "どうのつるぎ");
+            Sword ironsword = new Sword(name = "はがねのつるぎ");
+            Sword swordofROTO = new Sword(name = "ロトのつるぎ");
+            Axe bronsaxe = new Axe(name = "どうのおの");
+            Axe ironaxe = new Axe(name = "はがねのおの");
+            Axe axeofDEVIL = new Axe(name = "まじんのおの");
+            Spear bamboospear = new Spear(name = "たけやり");
+            Spear ironspear = new Spear(name = "はがねのやり");
+            Spear spearofLoneMetalSlime = new Spear(name = "はぐれメタルのやり");
+
         }
+
+        public class WeaponBase
+        {
+            public string name;
+            
+            public WeaponBase(string name)
+            {
+                this.name = name;
+            }
+        }
+
+
+        public class Sword : WeaponBase
+        {
+            public Sword(string name) :base (name) {}
+        }
+
+        public class Axe : WeaponBase
+        {
+            public Axe(string name) :base (name) {}
+        }
+
+        public class Spear : WeaponBase
+        {
+            public Spear(string name) :base (name) {}
+        }
+
+
     }
 }
