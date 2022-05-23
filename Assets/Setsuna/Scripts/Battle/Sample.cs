321e93890a19057826b442eed80215404dc7863c
sirokarasu <rockshooter3931@gmail.com>
2022-05-23 22:03:36 +0900

    出来た

--- Assets/Setsuna/Scripts/Battle/Sample.cs
+++ Assets/Setsuna/Scripts/Battle/Sample.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -32,15 +32,15 @@ namespace Setsuna
 
             List<WeaponBase> weaponlist = new List<WeaponBase>();
 
-            Sword bronzesword = new Sword(name = "どうのつるぎ");
-            Sword ironsword = new Sword(name = "はがねのつるぎ");
-            Sword swordofROTO = new Sword(name = "ロトのつるぎ");
-            Axe bronzeaxe = new Axe(name = "どうのおの");
-            Axe ironaxe = new Axe(name = "はがねのおの");
-            Axe axeofDEVIL = new Axe(name = "まじんのおの");
-            Spear bamboospear = new Spear(name = "たけやり");
-            Spear ironspear = new Spear(name = "はがねのやり");
-            Spear spearofLoneMetalSlime = new Spear(name = "はぐれメタルのやり");
+            Sword bronzesword = new Sword(name = "どうのつるぎ", true);
+            Sword ironsword = new Sword(name = "はがねのつるぎ", true);
+            Sword swordofROTO = new Sword(name = "ロトのつるぎ", false);
+            Axe bronzeaxe = new Axe(name = "どうのおの", true);
+            Axe ironaxe = new Axe(name = "はがねのおの", true);
+            Axe axeofDEVIL = new Axe(name = "まじんのおの", false);
+            Spear bamboospear = new Spear(name = "たけやり", true);
+            Spear ironspear = new Spear(name = "はがねのやり", true);
+            Spear spearofLoneMetalSlime = new Spear(name = "はぐれメタルのやり", false);
 
             weaponlist.Add(bronzesword);
             weaponlist.Add(ironsword);
@@ -52,13 +52,26 @@ namespace Setsuna
             weaponlist.Add(ironspear);
             weaponlist.Add(spearofLoneMetalSlime);
 
+            aaaa(weaponlist, Type.sword);
+            aaaa(weaponlist, Type.axe);
+            aaaa(weaponlist, Type.spear);
+
+        }
+
+        public enum Type
+        {
+            sword,
+            axe,
+            spear
         }
 
         public class WeaponBase
         {
             public string name;
+            public bool canbuy;
+            public Type type;
             
-            public WeaponBase(string name)
+            public WeaponBase(string name, bool canbuy)
             {
                 this.name = name;
             }
@@ -67,22 +80,72 @@ namespace Setsuna
 
         public class Sword : WeaponBase
         {
-            public Sword(string name) :base (name) {}
+            public Sword(string name, bool canbuy) :base (name, canbuy)
+            {
+                this.canbuy = canbuy;
+                type = Type.sword;
+            }
         }
 
         public class Axe : WeaponBase
         {
-            public Axe(string name) :base (name) {}
+            public Axe(string name, bool canbuy) :base (name, canbuy)
+            {
+                this.canbuy = canbuy;
+                type = Type.axe;
+            }
         }
 
         public class Spear : WeaponBase
         {
-            public Spear(string name) :base (name) {}
+            public Spear(string name, bool canbuy) :base (name, canbuy) 
+            {
+                this.canbuy = canbuy;
+                type = Type.spear;
+            }
         }
 
-        public class Name
+        /// <summary>
+        /// 武器の種類分けをしてリスト化、その後結果をstring変数に格納する
+        /// </summary>
+        /// <param name="weaponlist">引数で渡された武器リスト</param>
+        /// <param name="selecttype">今回ソートする武器の種類</param>
+        public void aaaa(List<WeaponBase> weaponlist, Type selecttype)
         {
-            int aaaa;
+
+            List<WeaponBase> weapon = weaponlist;
+            string A = "";//販売品を表示するための変数
+            string B = "";//非売品を(ry
+            //ソートしたい武器をselecttypeで抽出し、リスト化
+            weapon = weapon.Where(x =>x.type == selecttype).ToList();
+ 
+
+            for (int i = 0; i < weapon.Count; i++)
+            {
+                if (weapon[i].canbuy == true)
+                {
+                    A += weapon[i].name + ", ";
+                }
+                else
+                {
+                    B += weapon[i].name + ", ";
+                }
+            }
+
+            switch (selecttype)
+            {
+                case Type.sword:
+                Debug.Log($"種類「剣」　販売品　{A}非売品　{B}");
+                break;
+
+                case Type.axe:
+                Debug.Log($"種類「斧」　販売品  {A}非売品　{B}");
+                break;
+
+                case Type.spear:
+                Debug.Log($"種類「槍」　販売品  {A}非売品　{B}");
+                break;    
+            }
             
         }
 
