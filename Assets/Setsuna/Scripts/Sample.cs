92b5976532e38b8a0a18b678955bf2fe8e3fb357
doctor <doctor.dev.games@gmail.com>
2022-06-01 01:05:01 +0900

    修正

---
+++ Assets/Setsuna/Scripts/Sample.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Sample
+{
+    public class Sample : MonoBehaviour
+    {
+        public void Start()
+        {
+
+        }
+    }
+}
